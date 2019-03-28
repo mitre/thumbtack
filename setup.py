6105360289e0751385c784bcf955764c938ae81e
Jared <flamableconcrete@gmail.com>
2019-03-28 20:15:25 +0000

    Update development status to stable

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     classifiers=[
-        'Development Status :: 4',
+        'Development Status :: 5',
         'Intended Audience :: Developers',
         'Topic :: Security',
         'Topic :: System :: Filesystems',
