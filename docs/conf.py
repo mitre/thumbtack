faaa58761a23bb4d4bac9a75fc63d0fe3fd865b0
Jared <flamableconcrete@gmail.com>
2019-04-04 04:28:04 +0000

    Bump version: 0.1.2 → 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The MITRE Corporation'
 author = 'The MITRE Corporation'
 
 # The short X.Y version
-version = '0.1.2'
+version = '0.1.3'
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
