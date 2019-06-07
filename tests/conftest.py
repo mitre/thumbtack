e8b4ce35d39c6e4df02fd160a8e28a007a768f40
Jared Ondricek <jondricek@mitre.org>
2019-06-07 16:07:15 -0400

    Use explicit sqlite database in travis CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from thumbtack import create_app
 
 @pytest.fixture(scope='module')
 def test_client():
-    flask_app = create_app()
+    flask_app = create_app(database='database.db')
     dir_path = os.path.dirname(os.path.realpath(__file__))
     flask_app.config['IMAGE_DIR'] = os.path.join(dir_path, 'test_images')
     flask_app.config['MOUNT_DIR'] = 'thumbtack_test_mount_dir'
