0877df46fdf381ed79f736426498d686ad3ab305
Jared Ondricek <jondricek@mitre.org>
2020-11-30 23:57:54 -0500

    Bump version: 0.3.1 → 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = "2019-2020, The MITRE Corporation"
 author = "The MITRE Corporation"
 
 # The short X.Y version
-version = "0.3.1"
+version = "0.3.2"
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.3.2"
 
 
 # -- General configuration ---------------------------------------------------
